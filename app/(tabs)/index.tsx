3238a5f54e41009026afbb3af62ed75c85355b32
Andrew Zhukov <voland62@gmail.com>
2025-05-28 18:47:56 +0300

    some testing changes on main page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome, Andrew!</ThemedText>
+        <ThemedText type="title">Welcome, Andrew!!!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
